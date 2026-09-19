3d56a1d511a8a02ba9b50c539bc03ba0f56dcb3b
Nethmina De Silva <npdesilva27@gmail.com>
2026-09-19 12:21:02 +0530

    Update ElectricEngine.cs

--- ElectricEngine.cs
+++ ElectricEngine.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace AbstractFactoryVehicle
 {
-    public class ElectricEngine: IEngine
+    class ElectricEngine : IEngine
     {
         public void Start()
         {
-            Console.WriteLine("Gasoline engine started with a roar.");
+            Console.WriteLine("Electric Engine Started");
         }
     }
 }
