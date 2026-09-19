a1ca6e550bb54fcdee1bac26616b806d2b962bd6
Nethmina De Silva <npdesilva27@gmail.com>
2026-09-19 12:18:56 +0530

    Update ElectricVehicleFactory.cs

--- ElectricVehicleFactory.cs
+++ ElectricVehicleFactory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace AbstractFactoryVehicle
 {
-    public class GasolineVehicleFactory : IVehicleFactory
+    class ElectricVehicleFactory: IVehicleFactory
     {
-        public IVehicle CreateCar()   { return new GasolineCar(CreateEngine()); }
-        public IVehicle CreateTruck() { return new GasolineTruck(CreateEngine()); }
-        public IEngine CreateEngine() { return new GasolineEngine(); }
+        public IVehicle CreateCar()
+        {
+            return new ElectricCar();
+        }
+
+        public IVehicle CreateTruck()
+        {
+            return new ElectricTruck();
+        }
+
+        public IEngine CreateEngine()
+        {
+            return new ElectricEngine();
+        }
     }
 }
